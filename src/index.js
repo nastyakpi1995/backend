18f6930e162636e4abd6266c74da9193bd69561c
Nastya Krat <nastyakpi1995@gmail.com>
2022-03-13 12:38:40 +0200

    Catch unexpected errors and exit app gracefully

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ const app = require('./app')
 const logger = require('./config/logger')
 const config = require('./config/config')
 
-app.on(config.port, () => {
-    logger.info(`Listening to port ${config.port}`)
-});
+let server;
+
+app.on('ready', () => {
+    server = app.listen(config.port, () => {
+        logger.info(`Listening to port ${config.port}`)
+    })
+})
+
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info('Server Closed')
+            process.exit(1)
+        })
+
+    } else {
+        process.exit(1)
+    }
+}
+
+const unexpextedErrorHandler = error => {
+    logger.error(`Unexpexted error ${error}`)
+    exitHandler()
+}
+
+process.on('unhandledRejection', unexpextedErrorHandler)
+process.on('uncaughtException', unexpextedErrorHandler)
